a02852d9f7920264a6f2944f312c52e1713d1ec8
jared kantrowitz <jared.kantrowitz@gmail.com>
2017-01-31 01:43:01 -0500

    Now with module scoped params! Look how much easier to read it is :DDDD

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,66 +1,67 @@
 const Promise = require('bluebird')
 const MinHeap = require('./minheap')
 
-const whileTrue = (printer, logSources, heapNode) => logSources[heapNode.source].popAsync()
-  .then((m) => {
-    if (m) {
-      printer.print(m)
-      return whileTrue(printer, logSources, heapNode)
-    }
-    return console.info(`Drained: ${heapNode.source}`)
-  })
-
-const whileCurrTimestampLTENextMinTimestamp = (minheap, printer, logSources, currTimestamp, nextMinTimestamp, heapNode) => {
-  if (currTimestamp <= nextMinTimestamp) {
-    return logSources[heapNode.source].popAsync()
-      .then((log) => {
-        if (log) {
-          const logtime = log.date.getTime()
-          if (logtime <= nextMinTimestamp) {
-            printer.print(log)
-            currTimestamp = logtime
-            // continue looping
-            return whileCurrTimestampLTENextMinTimestamp(minheap, printer, logSources, currTimestamp, nextMinTimestamp, heapNode)
-          }
-          return minheap.insert({
-            ts: logtime,
-            source: heapNode.source,
-            data: log,
-          })
-        }
-        console.info(`Drained: ${heapNode.source}`)
-        heapNode = minheap.popMin()
-        printer.print(heapNode.data)
-        currTimestamp = heapNode.ts
-        if (minheap.heapList.length) {
-          nextMinTimestamp = minheap.getPeakTimestamp()
-          // continue looping
-          return whileCurrTimestampLTENextMinTimestamp(minheap, printer, logSources, currTimestamp, nextMinTimestamp, heapNode)
-        }
-
-        return whileTrue(printer, logSources, heapNode)
-      })
-  }
-
-  return 'All done!'
-}
-
-const whileHeapList = (minheap, printer, logSources) => {
-  if (!minheap.heapList.length) return 'All done'
-
-  const heapNode = minheap.popMin()
-  const currTimestamp = heapNode.ts
-  printer.print(heapNode.data)
-  const nextMinTimestamp = minheap.getPeakTimestamp()
-  return whileCurrTimestampLTENextMinTimestamp(minheap, printer, logSources, currTimestamp, nextMinTimestamp, heapNode)
-    .then(() => whileHeapList(minheap, printer, logSources))
-}
+const minheap = new MinHeap()
 
 module.exports = (logSources, printer) => {
-  const minheap = new MinHeap()
+  const whileTrue = heapNode => logSources[heapNode.source].popAsync()
+    .then((m) => {
+      if (m) {
+        printer.print(m)
+        return whileTrue(heapNode)
+      }
+      return console.info(`Drained: ${heapNode.source}`)
+    })
+
+  const whileCurrTimestampLTENextMinTimestamp = (currTimestamp, nextMinTimestamp, heapNode) => {
+    if (currTimestamp <= nextMinTimestamp) {
+      return logSources[heapNode.source].popAsync()
+        .then((log) => {
+          if (log) {
+            const logtime = log.date.getTime()
+            if (logtime <= nextMinTimestamp) {
+              printer.print(log)
+              currTimestamp = logtime
+              // continue looping
+              return whileCurrTimestampLTENextMinTimestamp(currTimestamp, nextMinTimestamp, heapNode)
+            }
+            return minheap.insert({
+              ts: logtime,
+              source: heapNode.source,
+              data: log,
+            })
+          }
+          console.info(`Drained: ${heapNode.source}`)
+          heapNode = minheap.popMin()
+          printer.print(heapNode.data)
+          currTimestamp = heapNode.ts
+          if (minheap.heapList.length) {
+            nextMinTimestamp = minheap.getPeakTimestamp()
+            // continue looping
+            return whileCurrTimestampLTENextMinTimestamp(currTimestamp, nextMinTimestamp, heapNode)
+          }
+
+          return whileTrue(heapNode)
+        })
+    }
+
+    return 'All done!'
+  }
+
+  const whileHeapList = () => {
+    if (!minheap.heapList.length) return 'All done'
+
+    const heapNode = minheap.popMin()
+    const currTimestamp = heapNode.ts
+    printer.print(heapNode.data)
+    const nextMinTimestamp = minheap.getPeakTimestamp()
+    return whileCurrTimestampLTENextMinTimestamp(currTimestamp, nextMinTimestamp, heapNode)
+      .then(() => whileHeapList())
+  }
+
   let i = 0
   Promise.map(logSources, logSource => logSource.popAsync())
     .map(log => minheap.insert({ ts: log.date.getTime(), source: i++, data: log }))
-    .then(() => whileHeapList(minheap, printer, logSources))
+    .then(() => whileHeapList())
     .then(() => printer.done())
 }
